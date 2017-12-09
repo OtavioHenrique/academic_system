e485cf768f5dc136399aa6288f97c7bb74841922
Otavio Henrique <otavio.henrique@hotmail.com>
2017-12-09 14:20:40 -0200

    mysql connection class

--- lib/database/mysql.rb
+++ lib/database/mysql.rb
@@ -1,5 +1,15 @@
+require 'mysql2'
+
 module Database
   class Mysql
-    
+    def initialize(host: , port: , username: , password: nil, database: )
+      Mysql2::Client.new(
+        host: host,
+        port: port,
+        username: username,
+        password: password,
+        database: database
+      )
+    end
   end
 end
