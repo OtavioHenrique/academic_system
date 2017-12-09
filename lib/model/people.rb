4a400f2949e10dc18097b7f0a78db6fe90b4e6d6
Otavio Henrique <otavio.henrique@hotmail.com>
2017-12-09 16:02:56 -0200

    Basic base at people model

--- lib/model/people.rb
+++ lib/model/people.rb
@@ -1,3 +1,9 @@
 class People
+  attr_reader :name, :last_name, :document
 
+  def initialize(name , last_name, document)
+    @name = name
+    @last_name = last_name
+    @document = document
+  end
 end
