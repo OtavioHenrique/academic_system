d4efb7a27c26a8eb7f2483e71e3576f83ce142ea
Otavio Henrique <otavio.henrique@hotmail.com>
2017-12-10 19:40:06 -0200

    Finished Employees model

--- lib/model/employee.rb
+++ lib/model/employee.rb
@@ -1,3 +1,13 @@
 class Employee < People
+  attr_reader :name, :last_name, :document, :sector,
+               :role, :salary
 
+  def initialize(name:, last_name:, document:,
+                  sector:, role:, salary:)
+
+    super(name, last_name, document)
+    @sector = sector
+    @role = role
+    @salary = salary
+  end
 end
