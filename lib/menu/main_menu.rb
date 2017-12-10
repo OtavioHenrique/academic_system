0bb27fbd4d6102ed6064b371d70361b7fe35d6d9
Otavio Henrique <otavio.henrique@hotmail.com>
2017-12-10 19:39:50 -0200

    Main Menu done / Calling employees sub menu

--- lib/menu/main_menu.rb
+++ lib/menu/main_menu.rb
@@ -1,9 +1,11 @@
+require 'artii'
+require 'colorize'
+
 module Menu
   class MainMenu
-    require 'artii'
     class << self
       def show
-        puts logo
+        puts logo.colorize(:blue)
 
         puts "Welcome to academic system\n\n\n"
 
@@ -11,9 +13,32 @@ module Menu
         puts "1 - Students"
         puts "2 - Employees"
         puts "3 - Professors"
+
+        print "Enter with your choice: ".colorize(:blue)
+        response = gets.chomp
+        case response
+        when "1"
+          students_menu
+        when "2"
+          employees_menu
+        when "3"
+          professors_menu
+        end
       end
 
       private
+
+      def students_menu
+        Students::MainMenu.show
+      end
+
+      def professors_menu
+        Professors::MainMenu.show
+      end
+
+      def employees_menu
+        Employees::MainMenu.show
+      end
 
       def logo
         a = Artii::Base.new
